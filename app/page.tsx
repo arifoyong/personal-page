146ec379059fde17f791f72caa95d4ad771927ef
Arif Oyong <arifoyong@gmail.com>
2025-11-23 12:19:10 +0800

    Cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
-import Chat from '@/components/chat-component/chat'
-import { generateConvId } from '@/app/action/conv.action'
+// import Chat from '@/components/chat-component/chat'
+// import { generateConvId } from '@/app/action/conv.action'
 import { Suspense } from "react";
 
 export default async function Home() {
-  const conv = await generateConvId()
+  // const conv = await generateConvId()
 
   return (
     <main className="flex flex-col container w-3/4 mx-auto max-h-screen">
       <Suspense fallback={<p>Loading...</p>}>
-        <Chat conversationId={conv.conversationId} />
+        {/* <Chat conversationId={conv.conversationId} /> */}
+        <p>test</p>
       </Suspense>
     </main>
   );
--- components/chat-component/disclaimer.tsx
+++
@@ -1,9 +0,0 @@
-import { my_name } from "@/app/data/profile";
-
-export const Disclaimer = () => {
-  return (
-    <div className='text-sm italic mt-2'>
-      {`⚠️ Disclaimer: This AI chatbot simulates ${my_name} for informational purposes. Responses are automatically generated and may not be accurate or up to date. For official information, please contact ${my_name} directly.`}
-    </div>
-  )
-}
